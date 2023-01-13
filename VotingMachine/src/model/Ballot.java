f942934769c004b280b74302a9159fd8011d2fc4
ksscott <scott.ksteven@gmail.com>
2023-01-12 17:02:14 -0700

    Fix set typing issues in records

--- VotingMachine/src/model/Ballot.java
+++ VotingMachine/src/model/Ballot.java
@@ -1,12 +1,10 @@
 package model;
 
-import org.apache.commons.collections4.set.UnmodifiableSet;
-
 import java.util.*;
 
-public record Ballot(String name, UnmodifiableSet<Race> races) {
+public record Ballot(String name, Set<Race> races) {
 
 	public Ballot(String name, Race... races) {
-		this(name, (UnmodifiableSet<Race>) Set.of(races));
+		this(name, Set.of(races));
 	}
 }
--- VotingMachine/src/model/Race.java
+++ VotingMachine/src/model/Race.java
@@ -1,17 +1,15 @@
 package model;
 
-import org.apache.commons.collections4.set.UnmodifiableSet;
-
 import java.util.Collection;
 import java.util.Set;
 
-public record Race(String name, UnmodifiableSet<Option> options) {
+public record Race(String name, Set<Option> options) {
 
     public Race(String name, Option... options) {
-        this(name, (UnmodifiableSet<Option>) Set.of(options));
+        this(name, Set.of(options));
     }
     
     public Race(String name, Collection<Option> options) {
-        this(name, (UnmodifiableSet<Option>) Set.copyOf(options));
+        this(name, Set.copyOf(options));
     }
 }
