c6258efe8b2f02a1db0909b106415d9719130da5
ksscott <scott.ksteven@gmail.com>
2023-02-21 09:25:36 -0700

    Add max players to each Game

--- VotingMachine/src/elections/games/Game.java
+++ VotingMachine/src/elections/games/Game.java
@@ -6,57 +6,61 @@ import java.util.List;
 import java.util.Optional;
 
 public enum Game {
-	ASTRONEER("Astroneer"),
-	BATTLEBLOCK_THEATER("BattleBlock Theater"),
-	BOOMERANG_FU("Boomerang Fu"),
-	CHESS("Chess"),
-	CIVILIZATION_6("Sid Meier's Civilization IV"),
-	COSMOTEER("Cosmoteer"),
-	DONT_STARVE_TOGETHER("Don't Starve Together"),
-	ECO("Eco"),
-	FACTORIO("Factorio"),
-	FIVE_D_CHESS("5D Chess"),
-	FOXHOLE("Foxhole"),
-	HALO("Halo"),
-	HEROES_OF_THE_STORM("Heroes of the Storm"),
-	HOMEWORLD("Homeworld"),
-	HUMANKIND("HUMANKIND"),
-	IT_TAKES_TWO("It Takes Two"),
-	KEEP_TALKING_AND_NOBODY_EXPLODES("Keep Talking and Nobody Explodes"),
-	KINGDOM_TWO_CROWNS("Kingdom Two Crowns"),
-	LEGION_TD_2("Legion TD 2"),
-	LOVERS_IN_A_DANGEROUS_SPACETIME("Lovers in a Dangerous Spacetime"),
-	MOVING_OUT("Moving Out"),
-	NEVER_SPLIT_THE_PARTY("Never Split the Party"),
-	NO_MANS_SKY("No Man's Sky"),
-	OVERCOOKED_2("Overcooked! 2"),
-	PLANETARY_ANNIHILATION_TITANS("Planetary Annihilation: TITANS"),
-	PORTAL_2("Portal 2"),
-	PUBG("PUBG: Battlegrounds"),
-	RAFT("Raft"),
-	RISK_OF_RAIN_2("Risk of Rain 2"),
-	SATISFACTORY("Satisfactory"),
-	SEVEN_DAYS_TO_DIE("7 Days to Die"),
-	SPACE_ENGINEERS("Space Engineers"),
-	STARCRAFT("StarCraft"),
-	STELLARIS("Stellaris"),
-	TABLETOP_SIMULATOR("Tabletop Simulator"),
-	TERRARIA("Terraria"),
-	TWELVE_ORBITS("12 orbits"),
-	ULTIMATE_CHICKEN_HORSE("Ultimate Chicken Horse"),
-	UNRAILED("Unrailed!"),
-	VAINGLORY("Vainglory"),
-	VALHEIM("Valheim"),
-	WORMS_WMD("Worms W.M.D."),
+	ASTRONEER("Astroneer", 4),
+	BATTLEBLOCK_THEATER("BattleBlock Theater", 4),
+	BOOMERANG_FU("Boomerang Fu", 6),
+	CHESS("Chess", 4), // 2, 4, infinite max players?
+	CIVILIZATION_6("Sid Meier's Civilization IV", 0),
+	COSMOTEER("Cosmoteer", 8),
+	DONT_STARVE_TOGETHER("Don't Starve Together", 6),
+	ECO("Eco", 0),
+	FACTORIO("Factorio", 0),
+	FIVE_D_CHESS("5D Chess", -1),
+	FOXHOLE("Foxhole", 0),
+	HALO("Halo", -1),
+	HEROES_OF_THE_STORM("Heroes of the Storm", 10),
+	HOMEWORLD("Homeworld", 8),
+	HUMANKIND("HUMANKIND", 8),
+	IT_TAKES_TWO("It Takes Two", 2),
+	KEEP_TALKING_AND_NOBODY_EXPLODES("Keep Talking and Nobody Explodes", 4),
+	KINGDOM_TWO_CROWNS("Kingdom Two Crowns", 2),
+	LEGION_TD_2("Legion TD 2", 4),
+	LOVERS_IN_A_DANGEROUS_SPACETIME("Lovers in a Dangerous Spacetime", 4),
+	MOVING_OUT("Moving Out", 4),
+	NEVER_SPLIT_THE_PARTY("Never Split the Party", 4),
+	NO_MANS_SKY("No Man's Sky", 32),
+	OVERCOOKED_2("Overcooked! 2", 4),
+	PLANETARY_ANNIHILATION_TITANS("Planetary Annihilation: TITANS", 10),
+	PORTAL_2("Portal 2", 2),
+	PUBG("PUBG: Battlegrounds", 4),
+	RAFT("Raft", 8),
+	RISK_OF_RAIN_2("Risk of Rain 2", 4),
+	SATISFACTORY("Satisfactory", 4),
+	SEVEN_DAYS_TO_DIE("7 Days to Die", 8),
+	SPACE_ENGINEERS("Space Engineers", 16),
+	STARCRAFT("StarCraft", 8),
+	STELLARIS("Stellaris", 0),
+	TABLETOP_SIMULATOR("Tabletop Simulator", 10),
+	TERRARIA("Terraria", 16),
+	TWELVE_ORBITS("12 orbits", 12),
+	ULTIMATE_CHICKEN_HORSE("Ultimate Chicken Horse", 4),
+	UNRAILED("Unrailed!", 4),
+	VAINGLORY("Vainglory", 10),
+	VALHEIM("Valheim", 10),
+	WORMS_WMD("Worms W.M.D.", 6),
 	;
 
 	public final String title;
+	public final int maxPlayers; // 0="unlimited"; -1="unknown"
 
-	Game(String title) {
+	Game(String title, int maxPlayers) {
 		this.title = title;
+		this.maxPlayers = maxPlayers;
 	}
 
 	public String getTitle() { return title; }
+
+	public int getMaxPlayers() { return maxPlayers; }
 
 	public static Optional<Game> interpret(String input) {
 		return Arrays.stream(values()).filter(game -> game.title.toLowerCase().contains(input.toLowerCase())).findAny();
